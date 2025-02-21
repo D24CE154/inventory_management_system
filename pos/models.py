09fba9a7d13812b9650e22d55d99a651a629cba0
D24CE154 <d24ce154@charusat.edu.in>
2025-02-21 18:53:00 +0530

    Created models for the database tables.

--- pos/models.py
+++ pos/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+
+from employees.models import Employee
+from inventory.models import Products, ProductItems
+
 
 # Create your models here.
+class Sales(models.Model):
+
+    paymentMethods = [('Cash'),('Card'),('UPI')]
+    sale_id = models.AutoField(primary_key=True)
+    employee_id = models.ForeignKey(Employee,null=False,on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(Customer,null=False,on_delete=models.CASCADE)
+    total_amount = models.DecimalField(max_digits=10,decimal_places=2,null=False,blank=False)
+    payment_method = models.CharField(choices=paymentMethods,max_length=10,null=False,blank=False)
+    sale_data = models.DateTimeField(null=False,blank=False,default=timezone.now)
+
+class SaleItems(models.Model):
+    saleitem_id = models.AutoField(primary_key=True)
+    sale_id = models.ForeignKey(Sales,null=False,on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Products,null=False,on_delete=models.CASCADE)
+    imei = models.ForeignKey(ProductItems,null=True,on_delete=models.SET_NULL)
+    quantity = models.IntegerField(null=False,blank=False)
+    selling_price = models.DecimalField(max_digits=10,decimal_places=2,null=False,blank=False)
+
+class Customers(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    customer_name = models.CharField(max_length=255,null=False,blank=False)
+    customer_address = models.CharField(max_length=255,null=False,blank=False)
+    customer_phone = models.CharField(max_length=255,null=False,blank=False)
