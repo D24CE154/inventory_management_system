9a2799996a81a392f22663e827b6f8e386cc7e4c
Samar Nilesh Chauhan <d24ce154@charusat.edu.in>
2025-02-23 14:18:28 +0530

    Update settings.py

--- inventory_management_system/settings.py
+++ inventory_management_system/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qj9g143ss0l$x_z6%1f+^x0ux^8v2&le@uv2f))0*mrza3n(a$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
